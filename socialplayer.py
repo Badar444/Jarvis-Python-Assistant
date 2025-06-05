6972cf9a5cad8838fd2952e703579b24cb6096dc
Badar444 <badar7587@gmail.com>
2025-06-05 10:42:27 +0500

    Add socialplayer with video links

---
+++ socialplayer.py
@@ -0,0 +1,23 @@
+# socialplayer.py
+
+"""
+This file contains video links used by Jarvis voice assistant.
+You can replace these sample video URLs with your own choice of videos.
+
+Currently, it includes some example Python tutorials, and funny shorts.
+"""
+
+video_links = {
+    # General Python tutorial videos
+    "python basics": "https://www.youtube.com/watch?v=_uQrJ0TkZlc",
+    "python oops": "https://www.youtube.com/watch?v=JeznW_7DlB0",
+    "python tutorial": "https://youtu.be/UrsmFxEIp5k?si=XsMcFOu5v9FJp92S",
+    "python advanced": "https://www.youtube.com/watch?v=HGOBQPFzWKo",
+
+    # Funny Python-related Shorts
+    "funny python 1": "https://youtube.com/shorts/h-yT_Kvelc4?si=IH043vUjslITM-h6",  
+    "funny python 2": "https://youtube.com/shorts/rMppOTNIrhw?si=xTuYpDCN9Q9CTGkZ",  
+    "funny python 3": "https://youtube.com/shorts/_ACW1yh5ltA?si=teBApPH0mPddTqz_", 
+
+
+}
